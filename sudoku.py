61fa7891019fe802895eecda945d3c4c452c00df
JP Lueck <137588989+hufihaf@users.noreply.github.com>
2025-05-17 18:34:38 -0700

    Update sudoku.py
    completed and updated the is_valid function

--- sudoku.py
+++ sudoku.py
@@ -15,20 +15,36 @@ def solve(board):
             board[row][column] = 0
     return False
 
+
 # 2d Array + Array -> Tuple
 def select_empty_cell(board, coords):
     pass
+
 
 # 2D Array -> Array
 def gather_empty_cells(board):
     pass
 
+
 # 2D Array + int + int + int -> bool
 def is_valid(board, num, row, column):
-    pass
+
+    # Check the row and column rule
+    for i in range (0,9):
+        if board[row][i] == num or board[i][column] == num:
+            return False
+        
+    # Check the block rule
+    block_row, block_col = row % 3, column % 3
+
+    for i in range(3):
+        for j in range(3):
+            if board[j + row - block_row][i + column - block_col] == num:
+                return False
+    return True
 
 # Test case
-board = [[0, 7, 0, 5, 3, 0, 9, 8, 0], 
+board = [[0, 7, 0, 5, 3, 0, 9, 8, 0],
          [0, 0, 0, 6, 0, 0, 2, 5, 7], 
          [5, 4, 0, 8, 2, 0, 6, 1, 0], 
          [1, 5, 0, 4, 0, 0, 8, 0, 2], 
@@ -37,4 +53,3 @@ board = [[0, 7, 0, 5, 3, 0, 9, 8, 0],
          [4, 1, 5, 9, 0, 2, 3, 7, 0], 
          [0, 6, 0, 0, 0, 0, 4, 0, 0], 
          [0, 0, 8, 0, 7, 0, 0, 6, 0]]
-print(solve(board))
